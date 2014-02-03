ed23b9814410fb1c17740a34064ba98cc9d35e2f
krisjhamilton <akstudiodevelopment@gmail.com>
2014-02-03 15:22:05 +1300

    Football.js

---
+++ football.js
@@ -0,0 +1,10 @@
+var footballJSON = 'http://pipes.yahoo.com/pipes/pipe.run?_id=de1f6d4f6cd25289cd15717107011dfe&_render=json';
+
+ajax({ url: footballJSON, type: 'json' }, function(data) {
+		simply.text({ 
+			title: data.value.items[0].title,
+			subtitle: data.value.items[0].description,
+			body: data.value.items[0].pubDate
+		});
+
+});
