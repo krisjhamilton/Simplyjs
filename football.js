ad9eb07ba11593e4f92f4ea0bf3ee47ef961850c
krisjhamilton <akstudiodevelopment@gmail.com>
2014-02-03 15:29:28 +1300

    Update football.js

--- football.js
+++ football.js
@@ -2,7 +2,7 @@ var footballJSON = 'http://pipes.yahoo.com/pipes/pipe.run?_id=de1f6d4f6cd25289cd
 simply.scrollable();
 ajax({ url: footballJSON, type: 'json' }, function(data) {
 		simply.text({ 
-			title: data.value.items[0].title,
+			//title: data.value.items[0].title,
 			subtitle: data.value.items[0].description,
 			body: data.value.items[0].pubDate
 		});
